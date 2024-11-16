35d715577723f7ca94564831c9e5444a71b1d48b
resultpanci <resultpanci@gmail.com>
2024-11-16 04:40:40 +0000

    resultpanci

--- index.py
+++ index.py
@@ -1,1 +1,20 @@
+x = int(input("Masukan sebuah bilangan : "))
+if x < 200:
+    print (x, " : Nilai kurang dari 200")
+    if x == 150:
+        keterangan = " : Sama dengan 150"
+        print (x, keterangan)
+    elif x == 100:
+        keterangan = " : Sama dengan 100"
+        print (x, keterangan)
+    elif x == 50:
+        keterangan = " : Sama dengan 50"
+        print (x, keterangan)
+    elif x < 50:
+        keterangan = " : Kurang dari 50"
+        print (x, keterangan)
+else:
+    keterangan = "Nilai yang dimasukkan salah"
+    print (keterangan)
 
+print ("Selesai")
